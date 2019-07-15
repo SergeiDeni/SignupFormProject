5271e81c74cbccbfb0af3c802070bee944ce70dc
Sergej <se.dzenisevich@gmail.com>
2019-07-15 14:49:35 +0300

    Login page start signup-component conslole log

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupFormComponent implements OnInit {
 
+  inputText1 = '';
+  inputText2 = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  save() {
+    console.log(this.inputText1);
+    console.log(this.inputText2)
+
+  }
+
+  onKeyUp1 (value){
+    // console.log(event)
+    this.inputText1=value;
+  }
+
+  onKeyUp2 (value){
+    // console.log(event)
+    this.inputText2=value;
+  }
 }
+
